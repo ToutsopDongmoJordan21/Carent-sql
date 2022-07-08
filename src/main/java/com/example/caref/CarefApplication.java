4cf06977b2026e13adf78b84a3b44915ed8dafee
ToutsopDongmoJordan21 <toutsopjordan1999@gmail.com>
2022-07-08 09:14:28 +0100

    first commit

---
+++ src/main/java/com/example/caref/CarefApplication.java
@@ -0,0 +1,13 @@
+package com.example.caref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caref/CarefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
